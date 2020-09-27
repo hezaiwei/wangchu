4e3a00ec3391f3cc1335b0ca400b181718bc32b5
zhou <1715322516@qq.com>
2020-09-27 21:59:53 +0800

    0927

--- wangchu/src/main/java/cn/hm/mapper/LabelMapper.java
+++ wangchu/src/main/java/cn/hm/mapper/LabelMapper.java
@@ -1,9 +1,11 @@
-package Mapper;
+package cn.hm.mapper;
 
 import cn.hm.bean.Label;
 
 import java.util.List;
 
 public interface LabelMapper {
-    List<Label> findAll();
+
+        List<Label> findAll();
+
 }
